6adb090101816ef22ab52477c735bcdc15618e9a
Crisdgomez <cristiandaniel.gomez@estudiantes.unahur.edu.ar>
2025-04-06 19:15:09 -0300

    Se agregan filtros pais insular y lengua guarani

--- paises.js
+++ paises.js
@@ -5,40 +5,43 @@ class FiltroPaises {
         return {
           pais: e.name.common,
           capital: e.capital !== undefined ? e.capital[0] : "Sin Capital",
-        };
-      };
-      this.formato = this.defaultFormato;
-      this.filtros = [];
-      try {
-        const response = await fetch("https://restcountries.com/v3.1/all");
-        this.data = await response.json();
-      } catch (ex) {
-        console.log(ex.message);
-        this.data = [];
+          fronteras:
+            e.borders !== undefined ? e.borders.join(", ") : "Sin Fronteras",
+          lenguaje: e.languages !== undefined ? e.languages : "Sin Lenguaje",
+        }
       }
-      return this;
-    })();
+      this.formato = this.defaultFormato
+      this.filtros = []
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all")
+        this.data = await response.json()
+      } catch (ex) {
+        console.log(ex.message)
+        this.data = []
+      }
+      return this
+    })()
   }
 
   addFiltro(name, fnFiltro) {
-    this.filtros.push({ name, fnFiltro });
+    this.filtros.push({ name, fnFiltro })
   }
 
   getFiltros() {
-    return this.filtros;
+    return this.filtros
   }
 
   setFormato(includePoputlation) {
-    this.formato = this.defaultFormato;
+    this.formato = this.defaultFormato
     if (includePoputlation)
       this.formato = (e) => {
-        return { ...this.defaultFormato(e), poblacion: e.population };
-      };
+        return { ...this.defaultFormato(e), poblacion: e.population }
+      }
   }
 
   aplicarFiltroConFormato(filterName) {
-    const filtro = this.filtros.find((f) => f.name === filterName);
-    if (filtro) return this.data.filter(filtro.fnFiltro).map(this.formato);
-    return [];
+    const filtro = this.filtros.find((f) => f.name === filterName)
+    if (filtro) return this.data.filter(filtro.fnFiltro).map(this.formato)
+    return []
   }
 }
